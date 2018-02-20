3a16aed68e35b79a5f60bbc7ea42646d9e9b27d0
Alex <lescot.alexg@gmail.com>
2018-02-20 11:22:52 +0100

    ajout de la recherche google

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+	
+def google(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/google311f29b3ed4bd3be.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
