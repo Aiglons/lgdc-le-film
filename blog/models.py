d733b1b20b25180d7df9ccd97e0413a7447b64b6
Alex <lescot.alexg@gmail.com>
2018-02-21 22:08:51 +0100

    ajout field image pour les articles

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    #image = models.ImageField(upload_to = 'images/', default = 'images/None/affiche-warriors-movie.jpg')
+    image_lien = models.CharField(max_length=400, default = 'http://www.croiseedesclans.fr/medias/images/affiche-warriors-movie.jpeg')
     resume = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(
